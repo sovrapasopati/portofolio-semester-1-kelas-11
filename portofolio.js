c1296623699e47d6c4d2eb34d6d82345d72cba4e
sovrapasopati <sovrakairi@gmail.com>
2025-12-04 01:38:41 -1100

    Add files via upload

---
+++ portofolio.js
@@ -0,0 +1,39 @@
+// dark and mode//
+const themeKey = "theme";
+
+window.onload = function () {
+    let body = document.body;
+    let tombol = document.getElementById("ganti-warna");
+
+    function setMode(mode) {
+        if (mode === "dark") {
+            body.classList.add("dark");
+            localStorage.setItem(themeKey, "dark")
+        } else {
+            body.classList.remove("dark");
+            localStorage.setItem(themeKey, "light");
+        }
+    }
+
+    tombol.addEventListener("click", function () {
+        const isDark = body.classList.contains("dark");
+
+        if (isDark) {
+            setMode("light");
+        } else {
+            setMode("dark");
+        }
+    });
+
+     const mode = localStorage.getItem(themeKey)
+    if(mode === "dark") {
+        setMode("dark")
+    } else {
+        setMode("light")
+    }
+};
+
+
+
+
+
